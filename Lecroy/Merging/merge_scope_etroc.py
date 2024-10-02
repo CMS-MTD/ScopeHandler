f7236fad7978b4136d091a92e32a870d49a501e0
gekobs <giac.zecc@gmail.com>
2024-10-02 18:01:12 +0200

    minor tweaks to get it operational

--- Lecroy/Merging/merge_scope_etroc.py
+++ Lecroy/Merging/merge_scope_etroc.py
@@ -25,7 +25,7 @@ CHANNEL_NUM = 1 #channel with square wave voltage (by index!! so subtract 1 to w
 OUTPUT_FILENAME = lambda run_num: f"run_{run_num}_rb0.root"
 BACKUP_FOLDER   = "/media/etl/Storage/LecroyMerged/"
 OUTPUT_FILE_DIR = f"{BASE_DATA_DIR}/ScopeData/LecroyMerged/"
-#OUTPUT_FILE_DIR = f"/home/etl/Test_Stand/ETL_TestingDAQ/ScopeHandler/Lecroy/Merging/unit_test/run_{f_index}_rb0.root"
+#OUTPUT_FILE_DIR = f"/home/etl/Test_Stand/ETL_TestingDAQ/ScopeHandler/Lecroy/Merging/unit_test/run_{run_number}_rb0.root"
 
 # STATUS FILEPATHS 
 NEXT_RUN_NUM_PATH               = f"{BASE_DATA_DIR}/Lecroy/Acquisition/next_run_number.txt"
@@ -63,36 +63,36 @@ def merge_trees(reco_data:akArray, scope_data:akArray, etroc_data:akArray, outpu
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run info.')
-    parser.add_argument('--runNumber',metavar='runNumber', type=str,default = -1, help='runNumber (default -1)',required=False)
+    parser.add_argument('--runNumber',metavar='runNumber', type=str,default = -1, help='runNumber (default -1) to start merging on.',required=False)
     parser.add_argument('--force', default=False, action='store_true', help='skip reco/merge/scope is running checks',required=False)
     parser.add_argument('--replace', default=False, action='store_true', help='If a merge file already exists, it deletes it and replaces it',required=False)
     parser.add_argument('--no_sym_link', default=False, action='store_true', help='Turns off sym linking to backup')
-    parser.add_argument('--auto', default=False, action='store_true', help='If needed files exists it will auto run and merge the data.')
+    parser.add_argument('--single_merge', default=False, action='store_true', help='Just merge script once')
     args = parser.parse_args()
 
     if args.runNumber==-1:
-        f_index=int(open(NEXT_RUN_NUM_PATH, "r").read())
+        run_number=int(open(NEXT_RUN_NUM_PATH, "r").read())
     else:
-        f_index = int(args.runNumber)
-    print(f"MERGING FILES FOR RUN: {f_index}")
+        run_number = int(args.runNumber)
 
-    main_loop = True
-    while main_loop:
-
+    file_not_found_cntr = 0
+    while file_not_found_cntr < 100:
+        print(f"----------MERGING FILES FOR RUN: {run_number}----------")
+        print(f"Number of consecutive files not found: {file_not_found_cntr}")
         #reco_loop(...)
 
         #root_dumper(...)
 
-        reco_path  = RECO_DATA_PATH(f_index) 
-        scope_path = SCOPE_DATA_PATH(f_index)
-        etroc_path = ETROC_DATA_PATH(f_index)
+        reco_path  = RECO_DATA_PATH(run_number) 
+        scope_path = SCOPE_DATA_PATH(run_number)
+        etroc_path = ETROC_DATA_PATH(run_number)
 
         reco  = file_status.is_data_ready('CONVERSION DATA', reco_path,  LECROY_CONVERSION_MERGING_PATH,  force=args.force)
         scope = file_status.is_data_ready('SCOPE DATA',      scope_path, LECROY_ACQUISITION_MERGING_PATH, force=args.force)
         etroc = file_status.is_data_ready('ETROC DATA',      etroc_path, MODULE_TEST_SW_MERGING,          force=args.force)
         
         merged_file = os.path.join(
-            OUTPUT_FILE_DIR, OUTPUT_FILENAME(f_index)
+            OUTPUT_FILE_DIR, OUTPUT_FILENAME(run_number)
         )
         print(f"Merged file: {merged_file} | Already Exist? {os.path.isfile(merged_file)}")
         if reco and scope and etroc:
@@ -108,16 +108,34 @@ if __name__ == "__main__":
             reco_data  = uproot.open(reco_path)[RECO_TREE].arrays()
             scope_data = uproot.open(scope_path)[SCOPE_TREE].arrays()
             etroc_data = uproot.open(etroc_path)[ETROC_TREE].arrays()
-            merge_trees(reco_data, scope_data, etroc_data, merged_file)
-            if not args.no_sym_link: #so if they give no_sym_link flag this doesnt run...
-                os.system(f"mv -f {merged_file} {BACKUP_FOLDER}")
-                os.system(f"ln -nsf {os.path.join(BACKUP_FOLDER, OUTPUT_FILENAME(f_index))} {merged_file}")
+            print("Checking array lengths")
+            print(f"Reco number of events:  {len(reco_data)}")
+            print(f"Scope number of events: {len(scope_data)}")
+            print(f"ETROC number of events: {len(etroc_data)}")
+            if not len(reco_data) == len(scope_data) == len(etroc_data):
+                print("Unequal event lengths, cannot merge.")
+            else:
+                merge_trees(reco_data, scope_data, etroc_data, merged_file)
+                if not args.no_sym_link: #so if they give no_sym_link flag this doesnt run...
+                    os.system(f"mv -f {merged_file} {BACKUP_FOLDER}")
+                    os.system(f"ln -nsf {os.path.join(BACKUP_FOLDER, OUTPUT_FILENAME(run_number))} {merged_file}")
 
-            if args.runNumber!=-1: break #if I am specifying the run to merge I am only doing that one
+            #if args.runNumber!=-1: break #if I am specifying the run to merge I am only doing that one
 
             # Set status to False so new data can be taken
             file_status.write_status(LECROY_CONVERSION_MERGING_PATH,  False)
             file_status.write_status(LECROY_ACQUISITION_MERGING_PATH, False)
             file_status.write_status(MODULE_TEST_SW_MERGING,          False)
+
+            run_number += 1 
+            file_not_found_cntr = 0 #reset counter
+        elif args.single_merge:
+            break
+        else:
+            run_number += 1 
+            file_not_found_cntr += 1
+
+        print('')
+
         #time.sleep(2)
         #main_loop = False
